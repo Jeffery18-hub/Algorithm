374b0c577064c1c4ce90702cddf233a8f11fc241
Jeffery18-hub <ljzmcs@gmail.com>
2023-05-13 08:55:51 +0800

    wildcard dp solution

--- leetcode/hard/44Wildcard_Matching.java
+++ leetcode/hard/44Wildcard_Matching.java
@@ -1,3 +1,4 @@
+// double pointers
 class Solution{
     boolean isMatch(String s, String p){
 	int i= 0;
@@ -31,3 +32,40 @@ class Solution{
 	return j==n;
     }
 }
+
+// dynamic programming
+
+class Solution{
+    boolean isMatch(String s, String p){
+	int m = s.length();
+	int n = p.length();
+
+	//定义二维数组，int array; init with value false
+	boolean[][] dp = new boolean[m+1][n+1];
+	
+	// 边界
+	dp[0][0]= true;
+
+	// s is empty and p string contains one or multi "*"
+	for(int i = 1; i<= n; ++i){
+	    if( p.charAt(i-1) == '*'){
+		dp[0][i]= dp[0][i-1];
+	    }
+	}
+
+	// 状态转移方程
+	for(int i=1; i <=m; ++i){
+	    for(int j=1; j<=n; ++j){
+		if(p.charAt(j-1) == '*'){
+		    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+		}
+		else{
+		    dp[i][j] = dp[i-1][j-1] && ( s.charAt(i-1)==p.charAt(j-1)  || p.charAt(j-1)== '?'); 
+		}	
+	    }
+	}
+
+	return dp[m][n];
+    }
+}
+
